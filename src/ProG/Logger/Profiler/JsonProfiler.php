1034f31d050ab1f1b016ab15e03f295ff1ea9bed
happyaccidents <hello@happyaccidents.me>
2013-07-02 12:14:27 +0100

    Added base json profiler

--- src/ProG/Logger/Profiler/JsonProfiler.php
+++ src/ProG/Logger/Profiler/JsonProfiler.php
@@ -10,11 +10,38 @@ class JsonProfiler implements ProfilerInterface
     use ConsoleInjectionTrait;
 
     /**
+     * @var array
+     */
+    protected $json;
+
+    /**
      * {@inheritdoc}
      */
     public function appendTo($content)
     {
+        if (! is_string($content)) {
+            throw new \InvalidArgumentException(
+                sprintf('%s expects to receive a string', __METHOD__)
+            );
+        }
 
+        $json = (array) json_decode($content);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \InvalidArgumentException(
+                sprintf('%s expects to receive a valid JSON string', __METHOD__)
+            );
+        }
+
+        $json['profiler'] = [
+            'queries'             => $this->console->getQueries(),
+            'timers'              => $this->console->getTimers(),
+            'memory_measurements' => $this->console->getMemoryMeasurements()
+        ];
+
+        $this->json = $json;
+
+        return $this;
     }
 
     /**
@@ -22,6 +49,6 @@ class JsonProfiler implements ProfilerInterface
      */
     public function render()
     {
-
+        return json_encode($this->json);
     }
 }
